c2cafa78e60a185afe68f8e8e524dc2d52d1ae27
cooler-SAI <coolerme86@gmail.com>
2025-04-09 20:22:56 +0300

    update simple code

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
 
-	fmt.Println("Hello gopher")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := fmt.Fprintf(w, "Hello Gopher from Kubernetes!")
+		if err != nil {
+			log.Printf("Error writing response: %v", err)
+			return
+		}
+		log.Printf("Received request from %s", r.RemoteAddr)
+	})
 
+	port := "8080"
+	log.Printf("Server starting on port %s\n", port)
+
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
 }
